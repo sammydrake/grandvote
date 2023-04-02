fa8633f9722ffe412abc62611430182a180eeb13
sammydrake <drakesammy0101@gmail.com>
2023-04-01 22:00:25 -0500

    n

--- context/contractAddress.js
+++ context/contractAddress.js
@@ -1,5 +1,7 @@
-import grandvote from "./Grandvote.json";
 
-export const contractAddress = "0x3d1FEeA14aD04F0d5C9943F9a09f6DBE67C86f9b";
-export const ownerAddress = "0x8e53E820C70D5f62821A5a81a0C0ff2cC84C0456";
-export const electionABI = grandvote.abi;
+  import grandvote from "./Grandvote.json"
+
+  export const contractAddress = "0x5503723BA43498570d1a116F6870ff37905F2329";
+  export const ownerAddress = "0x8e53E820C70D5f62821A5a81a0C0ff2cC84C0456";
+  export const electionABI = grandvote.abi;
+  
